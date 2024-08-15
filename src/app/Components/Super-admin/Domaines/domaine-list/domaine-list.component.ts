e34ef9bcd72ed9503bf5b80b19f77f0c9ae05629
DialloMaria <diallomariam0715@gmail.com>
2024-08-15 21:29:43 +0000

    Ajout du composant Liste des Domaines pour le Super-admin

---
+++ src/app/Components/Super-admin/Domaines/domaine-list/domaine-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-domaine-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './domaine-list.component.html',
+  styleUrl: './domaine-list.component.css'
+})
+export class DomaineListComponent {
+
+}
