b1e4e5b24ab0fd73bdb13380ff3d06b04c82e38f
DialloMaria <diallomariam0715@gmail.com>
2024-08-15 21:30:01 +0000

    Ajout du composant Liste des Événements pour le Super-admin

---
+++ src/app/Components/Super-admin/Evenements/evenement-list/evenement-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evenement-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './evenement-list.component.html',
+  styleUrl: './evenement-list.component.css'
+})
+export class EvenementListComponent {
+
+}
