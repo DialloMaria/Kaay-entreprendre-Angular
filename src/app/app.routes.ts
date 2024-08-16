74ce4b0616a71a6fa3a0d8ee3b50fb7eed2ce3f9
DialloMaria <diallomariam0715@gmail.com>
2024-08-16 01:45:36 +0000

    CReation des routes pour domaines

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DomaineListComponent } from './Components/Super-admin/Domaines/domaine-list/domaine-list.component';
+import { FormDomaineListComponent } from './Components/Super-admin/Domaines/form-domaine-list/form-domaine-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+// Redirection par défaut vers la liste des domaines
+  { path: '', redirectTo: '/domaines', pathMatch: 'full' },
+
+  // Liste des domaines
+  { path: 'domaines', component: DomaineListComponent },
+
+ // Ajout d'un nouveau domaine
+  { path: 'domaines/new', component: FormDomaineListComponent },
+
+ // Modification d'un domaine existant
+  { path: 'domaines/edit/:id', component: FormDomaineListComponent },
+
+];
