fd7e49548358a0c9275a5acf22f0555aead3f305
DialloMaria <diallomariam0715@gmail.com>
2024-08-16 01:05:33 +0000

    Ajout du composant form  des domaines pour le Super-admin

---
+++ src/app/Components/Super-admin/Domaines/form-domaine-list/form-domaine-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-domaine-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-domaine-list.component.html',
+  styleUrl: './form-domaine-list.component.css'
+})
+export class FormDomaineListComponent {
+
+}
